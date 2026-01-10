f1fb11f3049705db7ba2d89c69458a7bad50595d
omar <oa34894@gmail.com>
2026-01-10 13:33:24 +0200

    Database Created..

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-print("hello gitttt") 
+from flask import Flask, render_template, request, redirect
+from models import db,StudentModel
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+
+with app.app_context(): 
+    db.create_all()
+
+@app.route('/create', methods = ['GET','POST'])
+def create():
+    if request.method == 'GET':
+        return render_template('table_create.html')
+    
+app.run(host='localhost', port=5000)
